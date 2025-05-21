c1cbb49234067cf57961a8ed051fdad23bfb0ff9
practice <khushalkothiya5@gmail.com>
2025-05-21 15:46:52 +0530

    save

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import express from "express";
+
+const app = express();
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`0 server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongo db connection failed !!!", err);
+  });
