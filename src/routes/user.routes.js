487d896b197c173a97efae3d7ac813e6c6b672ad
practice <khushalkothiya5@gmail.com>
2025-05-23 17:09:08 +0530

    refresh token complete

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/usercontroller.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/usercontroller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -12,5 +18,9 @@ router.post(
   ]),
   registerUser
 );
+
+router.post("/login", loginUser);
+router.post("/logout", verifyJWT, logoutUser);
+router.post("/refresh-token", refreshAccessToken);
 
 export default router;
