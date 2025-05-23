48db899fee3555eccf6b92be15d70cac499c25f0
practice <khushalkothiya5@gmail.com>
2025-05-23 10:28:10 +0530

    save

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.urlencoded());
 app.use(express.static("public"));
 app.use(cookieParser());
 
-export { app };
+//routes imports
+import userRouter from "./routes/user.routes.js";
+
+//routes declarationd
+app.use("/api/v1/users", userRouter);
+
+export default app;
